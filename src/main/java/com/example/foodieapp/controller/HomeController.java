dbfbf58a85937c6ddeb400f12bc7959c1fab3a93
KrajAnna <krajanna2505@gmail.com>
2024-06-02 18:19:24 +0200

    Struktura projektu

--- src/main/java/com/example/foodieapp/controller/HomeController.java
+++ src/main/java/com/example/foodieapp/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.foodieapp;
+package com.example.foodieapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
